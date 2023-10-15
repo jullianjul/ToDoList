ad656ecbb89468be30b0f0feb1c33f5fc011490c
Julian Oviedo <you@example.com>
2023-10-15 18:16:51 -0500

    se agregaron muchas funcionalidades al login y el registro, ahora  multiples usuarios pueden ser guardados y su id se registra en la pagina, queda pendiente el comprobar si el email existe para no permitir la creación de un nuevo usuario con diferente email

--- ProyectofinalJovenesCreativosToDoList/src/Loginandregister.jsx
+++ ProyectofinalJovenesCreativosToDoList/src/Loginandregister.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './Loginandregister.css';
 import Input from './input';
-
+import { Register } from './licomponents/Register';
 
 
 const Loginandregister= () => {
@@ -14,6 +14,14 @@ const Loginandregister= () => {
     const formularioLogin = document.querySelector(".formulario__login");
     const formularioRegister = document.querySelector(".formulario__register");
     const contenedorLoginRegister = document.querySelector(".contenedor__login-register");
+    
+    const isLogged = localStorage.getItem('islog');
+    
+    if (isLogged === 'true') {
+      // Si está registrado, redirige a la página '/Aplication'
+      window.location.href = '/Aplication';
+    }
+
 
     const anchoPage = () => {
       if (window.innerWidth > 850) {
@@ -78,49 +86,31 @@ const Loginandregister= () => {
   {/*Fin animación*/}
 
   {/*recibidor de parametros*/}
-  
+  {/* parametros login*/}
   const [Email, setEmail]= useState('');
   const [password, setPassword]= useState('');
   const [passwordError, setPasswordError]= useState(false);
   const [isLogin, setIsLogin]= useState(false);
   const [hasError, setHasError]= useState(false);
-
+  const [registersuccess, setRegisterSuccess]= useState(false);
+  {/*fin parametros login*/}
+   {/*funciones login*/}
   function handleChange(attributes,Value){
     if (attributes.name === 'email'){
       setEmail(Value)
       setHasError(false)
+      setRegisterSuccess(false)
     } else if(attributes.name === 'Password'){
       if(Value.length <6){
         setPasswordError(true);
         setHasError(false)
+        setRegisterSuccess(false)
       }else{
         setPasswordError(false);
         setPassword(Value)
         setHasError(false)
+        setRegisterSuccess(false)
       }
-    }
-  }
-
-  function ifMatch(param){
-    if(param.Email.length > 0 && param.password.length > 0){
-      if(param.Email==='juliandavidoviedoramirez@gmail.com' && param.password=== '123456'){
-        const { Email, password}= param;
-        let ac= {Email, password};
-        let account = JSON.stringify(ac);
-        localStorage.setItem('account',account)
-        localStorage.setItem('islog',true)
-        setIsLogin(true);
-        setHasError(false);
-        window.location.href = '/Aplication';
-      } else{
-        setIsLogin(false);
-        setHasError(true);
-        localStorage.setItem('islog',false)
-      }
-    }else {
-      setIsLogin(false);
-      setHasError(true);
-      localStorage.setItem('islog',false)
     }
   }
 
@@ -130,8 +120,145 @@ const Loginandregister= () => {
       ifMatch(account)
     }
   } 
+
+  function ifMatch(accountuser) {
+    // Obtén el objeto de usuarios almacenados en localStorage
+    const usersData = JSON.parse(localStorage.getItem('usertotalinfo')) || [];
+    const matchingUser = usersData.find(user => user.EmailR === accountuser.Email && user.passwordR === accountuser.password);
+  
+    if (accountuser.Email.length > 0 && accountuser.password.length > 0 && matchingUser) {
+      const { EmailR, passwordR, NameR } = matchingUser;
+      let ac = { EmailR, passwordR,NameR };
+      let account = JSON.stringify(ac);
+      localStorage.setItem('account', account);
+      localStorage.setItem('islog', true);
+      setIsLogin(true);
+      setHasError(false);
+  
+      console.log('Usuario logueado:', NameR);
+
+  
+      window.location.href = '/Aplication';
+    } else {
+      setIsLogin(false);
+      setHasError(true);
+      console.log('Credenciales incorrectas o usuario no encontrado');
+    }
+  }
+  
+
+  {/*fin parametros login*/}
+
+  {/*funciones register*/}
+  
+  const [EmailR, setEmailr]= useState('');
+  const [passwordR, setPasswordr]= useState('');
+  const [NameR, setNamer]= useState('');
+  const [username, setUserName]= useState('');
+  const [passwordErrorR, setPasswordErrorr]= useState(false);
+  const [EmailError, setEmailError]= useState(false);
+  const [hasErrorR, setHasErrorr]= useState(false);
+  const [UserError, setUserError]= useState(false)
+
+
+
+
+  function handleChangeregister(attributes,Value){
+    setRegisterSuccess(false)
+    if(attributes.name==='Nameuser'){
+      setNamer(Value)
+      setHasErrorr(false)
+    }
+    if (attributes.name === 'Emailr') {
+      if (Value.includes('@')) {
+        // Verifica si el email contiene el símbolo "@"
+        if (Value.length < 5) {
+          // Verifica si el email tiene una longitud mínima (5 caracteres en este ejemplo)
+          setEmailError(true);
+          console.log('El email debe ser más largo');
+        } else {
+          // El email contiene "@" y tiene longitud suficiente
+          setEmailError(false);
+          setEmailr(Value);
+          console.log('El email es válido');
+        }
+      } else {
+        // El email no contiene el símbolo "@"
+        setEmailError(true);
+        console.log('El email no es válido');
+        setHasErrorr(false)
+      }
+    }
+
+    if(attributes.name==='Usernamer'){
+      //comprueba si el user tiene mas de 6 caracteres
+      setUserName(Value);
+      setHasErrorr(false);
+      if(username.length<5){
+        setUserError(true);
+      }else{
+        setUserError(false);
+      }
+    }
+    if(attributes.name==='Passworduser'){
+      setHasErrorr(false)
+      if(Value.length<6){
+        setPasswordErrorr(true);
+      }else{
+        setPasswordr(Value)
+        setPasswordErrorr(false);
+
+      }
+    }
+  }
+  console.log('contraseña',passwordR,passwordErrorR)
+
+  function handleSubmitregister(){
+    if((EmailR!=='')||(passwordR!=='')||(username!=='')||(NameR!=='')){
+      if((EmailError===true) || (passwordErrorR===true) || (UserError===true)){
+        console.log('papi, llene bien los campos')
+        setHasErrorr(true)
+      }else{
+        let usertotalinfo={EmailR,passwordR,NameR,username}
+        console.log('user:', usertotalinfo)
+        if(usertotalinfo){
+        ifmatches(usertotalinfo)
+        }
+        
+      }
+    }else{
+      console.log('XD dejaba los campos vacios')
+      setHasErrorr(true)
+    }
+  }
+
+  function ifmatches(props) {
+    const { EmailR, passwordR, NameR, username } = props;
+  
+    // Paso 1: Obtén el valor actual del localStorage o inicializa un arreglo vacío si no existe
+    const existingData = JSON.parse(localStorage.getItem('usertotalinfo')) || [];
+    // Paso 2: Crea un nuevo objeto con los datos y se agrega al arreglo
+    const newUserInfo = { EmailR, passwordR, NameR, username };
+    existingData.push(newUserInfo);
+  
+    // Paso 3: Guarda el arreglo actualizado en el localStorage
+    localStorage.setItem('usertotalinfo', JSON.stringify(existingData));
+    console.log('papi se te guardo')
+    setRegisterSuccess(true);
+  }
+  
+  {/*fin funciones register*/}
+
+
+
   return (
     <div className="LoginAndRegister">
+      {registersuccess &&
+      <h1 className='userloged'>felicitaciones, te has registrado, ahora puedes iniciar sesión</h1>
+      }
+      {hasErrorR &&
+          <h1>Porfavor, llene bien los campos para poder ser registrado</h1>
+      }
         <main>
           <div className="contenedor__todo">
             <div className="caja__trasera">
@@ -165,7 +292,7 @@ const Loginandregister= () => {
                   id:'Pass',
                   name: 'Password',
                   type: 'Password',
-                  placeholder: 'Ingrese su contraseña'
+                  placeholder: 'Ingrese su contraseña',
                 }
                 }
                 handleChange={handleChange}
@@ -178,38 +305,50 @@ const Loginandregister= () => {
               </form> 
               <form className="formulario__register">
                 <h2>Regístrarse</h2>
-                <Input attributes={{
+                <Register attributes={{
                   id:'Nameuserid',
                   name: 'Nameuser',
                   type: 'text',
-                  placeholder: 'Ingrese su Nombre'
+                  placeholder: 'Ingrese su Nombre',
+                  Check: 'input_Style'
                 }
-                }handleChange={handleChange} />
-                <Input attributes={{
+                }handleChangeregister={handleChangeregister} />
+                <Register attributes={{
                   id:'Correousuario',
-                  name: 'Email',
+                  name: 'Emailr',
                   type: 'text',
-                  placeholder: 'Ingrese su Email'
+                  placeholder: 'Ingrese su Email',
+                  Check: EmailError ? 'input-error' : 'input_Style'
                 }
-                } handleChange={handleChange} />
-                  <Input attributes={{
+                } handleChangeregister={handleChangeregister} 
+                />
+                {EmailError && 
+                <label htmlFor="" className='label-error'>Debe ingresar un correo valido</label>
+                }
+                  <Register attributes={{
                   id:'Username',
-                  name: 'Username',
+                  name: 'Usernamer',
                   type: 'text',
-                  placeholder: 'Ingrese su Nombre de usuario'
+                  placeholder: 'Ingrese su Nombre de usuario',
+                  Check: UserError ? 'input-error' : 'input_Style'
                 }
-                } handleChange={handleChange} />
-                    <Input attributes={{
+                } handleChangeregister={handleChangeregister} />
+                {UserError && 
+                <label htmlFor="" className='label-error'>Su usuario debe ser de 5 caracteres o más</label>
+                }
+                    <Register attributes={{
                   id:'Password',
                   name: 'Passworduser',
                   type: 'password',
-                  placeholder: 'Ingrese una Contraseña'
+                  placeholder: 'Ingrese una Contraseña',
+                  Check: passwordErrorR ? 'input-error' : 'input_Style'
                 }
-                } handleChange={handleChange} />
-                {passwordError && 
+                } handleChangeregister={handleChangeregister} 
+                />
+                {passwordErrorR && 
                 <label htmlFor="" className='label-error'>contraseña invalida o incompleta</label>
                 }
-                <button>Regístrarse</button>
+                <input type='button' onClick={handleSubmitregister} value={'Entrar'}  className='botonentrar'/>
               </form>
             </div>
           </div>
---
+++ ProyectofinalJovenesCreativosToDoList/src/licomponents/Register.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Register = ({attributes, handleChangeregister, props}) => {
+  return (
+    <input
+    id={attributes.id}
+    name={attributes.name}
+    placeholder={attributes.placeholder}
+    type={attributes.type}
+    onChange={ (e) => handleChangeregister(e.target, e.target.value)}
+    className={attributes.Check}
+   />
+  )
+}
